363a02f2e80fb2b4b1016a88e3ee7ea423c99e33
InvertDew <justinleeoglesby@gmail.com>
2022-09-14 19:34:28 -0500

    next commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          BRUH
+          BRUH I TEST
         </p>
       </header>
     </div>
